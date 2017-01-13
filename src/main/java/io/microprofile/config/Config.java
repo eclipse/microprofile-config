69747975d878f6469ab316bf6f05c6dfd8939293
Werner Keil <werner.keil@gmx.net>
2017-01-13 21:11:26 +0100

    Updated license header, author based on fix from https://github.com/microprofile/microprofile-config/issues/7

--- src/main/java/io/microprofile/config/Config.java
+++ src/main/java/io/microprofile/config/Config.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corp. and others
+ * Copyright (c) 2016, 2017 IBM Corp. and others
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ import java.util.Optional;
  * 
  * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
  * @author <a href="mailto:emijiang@uk.ibm.com">Emily Jiang</a>
+ * @author <a href=mailto:werner@catmedia.site">Werner Keil</a>
  *
  */
 public interface Config {	
