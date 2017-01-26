b97c275521ed7f803f5d4cce91b6b7b78b04d4fd
Mark Struberg <struberg@apache.org>
2017-01-26 16:16:39 +0100

    fix copyright headers according to Eclipse requirements
    so they say at least...

--- tck/src/main/java/io/microprofile/config/tck/ConfigProviderTest.java
+++ tck/src/main/java/io/microprofile/config/tck/ConfigProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Original Author and authors, IBM Corp and others
+ * Copyright (c) 2016-2017 Mark Struberg and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
  */
 package io.microprofile.config.tck;
 
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Map;
 import java.util.Properties;
 
@@ -32,24 +30,6 @@ import org.testng.annotations.Test;
  * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
  */
 public class ConfigProviderTest {
-
-    @Test
-    public void testConfigProviderWithDefaultTCCL() {
-        ClassLoader oldTccl = Thread.currentThread().getContextClassLoader();
-        try {
-            ClassLoader tempCl = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
-            Thread.currentThread().setContextClassLoader(tempCl);
-            Config config = ConfigProvider.getConfig();
-            Assert.assertNotNull(config);
-
-            Config config2 = ConfigProvider.getConfig(tempCl);
-            Assert.assertNotNull(config2);
-            Assert.assertEquals(config, config2);
-        }
-        finally {
-            Thread.currentThread().setContextClassLoader(oldTccl);
-        }
-    }
 
     @Test
     public void testEnvironmentConfigSource() {
@@ -92,59 +72,4 @@ public class ConfigProviderTest {
         Assert.assertNull(config.getString("tck.config.test.keydoesnotexist").get());
     }
 
-/* TODO important for Gunnars use case!
-    @Test
-    public void testRegisterManualConfig() {
-        // make sure we get a clean config
-        ConfigProvider.releaseConfig(ConfigProvider.getConfig());
-
-        ConfigProvider.registerConfig()
-                .ignoreDefaultSources()
-                .withSources(new SampleYamlConfigSource(null))
-                .build();
-
-        Config config = ConfigProvider.getConfig();
-        Assert.assertNotNull(config);
-        Assert.assertNotNull(config.getConfigSources());
-        Assert.assertEquals(1, config.getConfigSources().length);
-        Assert.assertEquals(SampleYamlConfigSource.class, config.getConfigSources()[0].getClass());
-
-        Assert.assertEquals("yamlvalue1", config.getValue("tck.config.test.sampleyaml.key1"));
-
-        Assert.assertNull(config.getValue("tck.config.test.javaconfig.properties.key1"));
-
-        {
-            // try it again, Sam
-            // this time we should fail as we already have a Config registered for this application
-            try {
-                ConfigProvider.registerConfig()
-                        .ignoreDefaultSources()
-                        .withSources(new SampleYamlConfigSource(null))
-                        .build();
-
-                Assert.fail("We fail because subsequently registering another Config for the application is not allowed");
-            }
-            catch (IllegalStateException ise) {
-                // all fine
-            }
-        }
-
-        // clean up the dirt afterwards
-        ConfigProvider.releaseConfig(ConfigProvider.getConfig());
-    }
-*/
-
-    @Test
-    public void testConfigProviderRelease() {
-        Config config1 = ConfigProvider.getConfig();
-        Config config2 = ConfigProvider.getConfig();
-
-        Assert.assertEquals(config2, config1);
-
-        ConfigProvider.releaseConfig(config2);
-
-        Config config3 = ConfigProvider.getConfig();
-
-        Assert.assertNotEquals(config1, config3);
-    }
 }
