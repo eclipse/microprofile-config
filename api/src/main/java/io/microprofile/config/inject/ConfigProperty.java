83b8fd55edbdb06c53401812676ef229f2f5b32b
ondrejm <ondro@ondro-lenovo-flex>
2017-02-22 01:01:13 +0100

    Adding description of how a default config key is generated.
    Signed-off-by: Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- api/src/main/java/io/microprofile/config/inject/ConfigProperty.java
+++ api/src/main/java/io/microprofile/config/inject/ConfigProperty.java
@@ -62,8 +62,12 @@ import javax.inject.Qualifier;
 @Target({METHOD, FIELD, PARAMETER, TYPE})
 public @interface ConfigProperty {
     /**
+     * The kay of the config property used to look up the configuration value. 
+     * If it is not specified, it will be derived automatically as{@code <class_name>.<injetion_point_name>}, 
+     * where {@code injection_point_name} is either a field name or a property name in case of field/property injection. Otherwise the value has to be provided.
+     * 
      * @return Name (key) of the config property to inject
      */
     @Nonbinding
-    String value();
+    String value() default "";
 }
