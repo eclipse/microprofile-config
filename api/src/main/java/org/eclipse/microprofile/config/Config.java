04fb80380c69fc131b423ca439709bc26be903d1
Gunnar Morling <gunnar.morling@googlemail.com>
2017-03-03 15:55:44 +0100

    Splitting up Config#getValue() into getValue() and getOptionalValue()
    Signed-off-by: Gunnar Morling <gunnar.morling@googlemail.com>

--- api/src/main/java/org/eclipse/microprofile/config/Config.java
+++ api/src/main/java/org/eclipse/microprofile/config/Config.java
@@ -43,15 +43,17 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
  *
  * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
  * @author <a href="mailto:emijiang@uk.ibm.com">Emily Jiang</a>
+ * @author <a href="mailto:gunnar@hibernate.org">Gunnar Morling</a>
  *
  */
 public interface Config {
+
     /**
      * Return the resolved property value with the specified type for the
      * specified property name from the underlying {@link ConfigSource ConfigSources}.
      *
      * If this method gets used very often then consider to locally store the configured value.
-     * 
+     *
      * @param <T>
      *             the property type
      * @param propertyName
@@ -59,9 +61,28 @@ public interface Config {
      * @param propertyType
      *             The type into which the resolve property value should get converted
      * @return the resolved property value as an Optional of the requested type.
-     * The {@link IllegalArgumentException} will be thrown if the property cannot be converted to the specified type.
+     * @throws IllegalArgumentException if the property cannot be converted to the specified type.
+     * @throws IllegalStateException if the property isn't present in the configuration.
      */
-    <T> Optional<T> getValue(String propertyName, Class<T> propertyType);
+    <T> T getValue(String propertyName, Class<T> propertyType);
+
+    /**
+     * Return the resolved property value with the specified type for the
+     * specified property name from the underlying {@link ConfigSource ConfigSources}.
+     *
+     * If this method gets used very often then consider to locally store the configured value.
+     *
+     * @param <T>
+     *             the property type
+     * @param propertyName
+     *             The configuration propertyName.
+     * @param propertyType
+     *             The type into which the resolve property value should get converted
+     * @return the resolved property value as an Optional of the requested type.
+     *
+     * @throws IllegalArgumentException if the property cannot be converted to the specified type.
+     */
+    <T> Optional<T> getOptionalValue(String propertyName, Class<T> propertyType);
 
     /**
      * Get an Optional raw string value associated with the given configuration
