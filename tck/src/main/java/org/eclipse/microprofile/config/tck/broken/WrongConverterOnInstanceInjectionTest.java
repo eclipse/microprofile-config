772aacb11778a11c3d9cbe1c85c878e27be1d290
Mark Struberg <struberg@apache.org>
2017-03-21 14:16:35 +0100

    add test to verify detection of wrong config format
    Signed-off-by: Mark Struberg <struberg@apache.org>

---
+++ tck/src/main/java/org/eclipse/microprofile/config/tck/broken/WrongConverterOnInstanceInjectionTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016-2017 Mark Struberg and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.eclipse.microprofile.config.tck.broken;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.spi.DeploymentException;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+/**
+ * Verify that injectng a native value also has a valid converter which can handle the configured string.
+ *
+ * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
+ */
+public class WrongConverterOnInstanceInjectionTest extends Arquillian {
+
+    @ShouldThrowException(DeploymentException.class)
+    @Deployment
+    public static WebArchive deploy() {
+        JavaArchive testJar = ShrinkWrap
+                .create(JavaArchive.class, "wrongConverterOnInstanceInjectionTest.jar")
+                .addClass(ConfigOwner.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource(new StringAsset("my.long.value=xxxxx"), "microprofile-config.properties")
+                .as(JavaArchive.class);
+
+        WebArchive war = ShrinkWrap
+                .create(WebArchive.class, "wrongConverterOnInstanceInjectionTest.war")
+                .addAsLibrary(testJar);
+        return war;
+    }
+
+    @Test
+    public void test() {
+    }
+
+
+    @RequestScoped
+    public static class ConfigOwner {
+
+
+        @Inject
+        @ConfigProperty(name="my.long.value")
+        private Long configValue;
+    }
+}
