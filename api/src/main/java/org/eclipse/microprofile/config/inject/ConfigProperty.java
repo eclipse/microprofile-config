1c0497b5f9ee4791ba66367a87a707b440ac2a6b
Mark Struberg <struberg@apache.org>
2017-04-13 22:05:26 +0200

    refs: #45 move to clarified license header
    Copyright holders are now in NOTICE as long as this file
    gets shipped.
    Note that the Copyright claim is exclusively only on the parts
    each author originary wrote himself and not granted to others
    except under the License.
    
    Signed-off-by: Mark Struberg <struberg@apache.org>

--- api/src/main/java/org/eclipse/microprofile/config/inject/ConfigProperty.java
+++ api/src/main/java/org/eclipse/microprofile/config/inject/ConfigProperty.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2016-2017 Payara Services Ltd., IBM Corp. and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
--- api/src/main/java/org/eclipse/microprofile/config/spi/ConfigProviderResolver.java
+++ api/src/main/java/org/eclipse/microprofile/config/spi/ConfigProviderResolver.java
@@ -3,7 +3,7 @@
  * Copyright (c) 2016 IBM Corp. and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
