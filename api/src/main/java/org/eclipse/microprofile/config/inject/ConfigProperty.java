ec95a56866b375ccd81ee457b050b4be55500fc7
Mark Struberg <struberg@apache.org>
2017-04-15 13:38:13 +0200

    Revert "#45 Updated license headers which had attribution to Eclipse committers"
    This reverts commit a2571425cc6a2e293b7f209ada32932737fd5b78.
    Sadly the proposed license header doesn't contain all legally
    required text for ALv2 and thus breaks the build.
    Please run mvn clean install to verify for each commit.

--- api/src/main/java/org/eclipse/microprofile/config/inject/ConfigProperty.java
+++ api/src/main/java/org/eclipse/microprofile/config/inject/ConfigProperty.java
@@ -1,16 +1,19 @@
-/**********************************************************************
- * Copyright (c) 2016-2017 Contributors to the Eclipse Foundation 
+/*
+ * Copyright (c) 2016-2017 Payara Services Ltd., IBM Corp. and others
  *
- * See the NOTICES file(s) distributed with this work for additional
- * information regarding copyright ownership.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Apache License, Version 2.0
- * which accompanies this distribution and is available at
- * http://www.opensource.org/licenses/apache2.0.php
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-License-Identifier: Apache-2.0
- **********************************************************************/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 package org.eclipse.microprofile.config.inject;
 
