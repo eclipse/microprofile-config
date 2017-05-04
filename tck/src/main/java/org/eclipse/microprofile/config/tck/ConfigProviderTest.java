7a1ed777ebcaa5ac8163323958d410772449ba30
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-05-04 11:27:49 +0200

    Standardize imports (avoid package import) As per comment in #127 Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com> Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- tck/src/main/java/org/eclipse/microprofile/config/tck/ConfigProviderTest.java
+++ tck/src/main/java/org/eclipse/microprofile/config/tck/ConfigProviderTest.java
@@ -18,7 +18,11 @@
  */
 package org.eclipse.microprofile.config.tck;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Properties;
