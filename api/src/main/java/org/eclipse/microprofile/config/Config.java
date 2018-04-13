b1d14a88a191a9e9be97990da0ef614e4fbac340
Emily Jiang <emijiang@uk.ibm.com>
2018-04-13 10:16:08 +0100

    #320 put back the version changes after the api restore
    Signed-off-by: Emily Jiang <emijiang@uk.ibm.com>

--- api/src/main/java/org/eclipse/microprofile/config/Config.java
+++ api/src/main/java/org/eclipse/microprofile/config/Config.java
@@ -89,13 +89,13 @@ public interface Config {
      * If this method gets used very often then consider to locally store the configured value.
      *
      * @param <T>
-     *             the property type
+     *             The property type
      * @param propertyName
      *             The configuration propertyName.
      * @param propertyType
      *             The type into which the resolve property value should get converted
      * @return the resolved property value as an object of the requested type.
-     * @throws IllegalArgumentException if the property cannot be converted to the specified type.
+     * @throws java.lang.IllegalArgumentException if the property cannot be converted to the specified type.
      * @throws java.util.NoSuchElementException if the property isn't present in the configuration.
      */
     <T> T getValue(String propertyName, Class<T> propertyType);
@@ -107,14 +107,14 @@ public interface Config {
      * If this method is used very often then consider to locally store the configured value.
      *
      * @param <T>
-     *             the property type
+     *             The property type
      * @param propertyName
      *             The configuration propertyName.
      * @param propertyType
      *             The type into which the resolve property value should be converted
-     * @return the resolved property value as an Optional of the requested type.
+     * @return The resolved property value as an Optional of the requested type.
      *
-     * @throws IllegalArgumentException if the property cannot be converted to the specified type.
+     * @throws java.lang.IllegalArgumentException if the property cannot be converted to the specified type.
      */
     <T> Optional<T> getOptionalValue(String propertyName, Class<T> propertyType);
 
