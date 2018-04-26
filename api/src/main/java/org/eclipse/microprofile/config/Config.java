38fb308eba072a39881a5885cf2fc2d69a1611a9
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2018-04-26 15:30:47 +0200

    Clarified priority of converters Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@payara.fish>

--- api/src/main/java/org/eclipse/microprofile/config/Config.java
+++ api/src/main/java/org/eclipse/microprofile/config/Config.java
@@ -47,8 +47,8 @@ import org.eclipse.microprofile.config.spi.ConfigSource;
  * The config objects produced via the injection model <pre>@Inject Config</pre> are guaranteed to be serializable, while
  * the programmatically created ones are not required to be serializable.
  * <p>
- * If one or more converters are registered for a class of a requested value then one of the registered converters
- * is used to convert the string value retrieved from config sources. 
+ * If one or more converters are registered for a class of a requested value then one of the registered converters 
+ * which has the highest priority is used to convert the string value retrieved from config sources. The highest priority means the highest priority number.
  * For more information about converters, see {@link org.eclipse.microprofile.config.spi.Converter}
  *
  * <h3>Usage</h3>
