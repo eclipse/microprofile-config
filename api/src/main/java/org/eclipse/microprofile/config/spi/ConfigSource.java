ef9b687904b5cf6b455353e140f7fb2149a22705
Emily Jiang <emijiang6@googlemail.com>
2019-12-05 17:47:24 +0000

    #431 further changes to maintain backward compatiblility
    Signed-off-by: Emily Jiang <emijiang6@googlemail.com>

--- api/src/main/java/org/eclipse/microprofile/config/spi/ConfigSource.java
+++ api/src/main/java/org/eclipse/microprofile/config/spi/ConfigSource.java
@@ -28,7 +28,9 @@
  *******************************************************************************/
 package org.eclipse.microprofile.config.spi;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -83,63 +85,75 @@ public interface ConfigSource {
      * @return the map containing the properties in this config source
      */
     default Map<String, String> getProperties() {
-        Map<String, String> props = new HashMap<>();
+        final Map<String, String> props = new HashMap<>();
         getPropertyNames().stream().forEach((prop) -> props.put(prop, getValue(prop)));
         return props;
     }
 
     /**
-     * Gets all property names known to this config source, potentially without evaluating the values.
+     * Gets all property names known to this config source, potentially without
+     * evaluating the values.
      *
-     * For backwards compatibility, there is a default implementation that just returns the keys of {@code getProperties()}
-     * slower ConfigSource implementations should replace this with a more performant implementation
+     * For backwards compatibility, there is a default implementation that just returns
+     * an empty set. This method should be overriden.
      *
      * @return the set of property keys that are known to this ConfigSource
      */
-    Set<String> getPropertyNames();
+    default Set<String> getPropertyNames() {
+        return Collections.<String>emptySet();
+    };
 
     /**
-     * Return the ordinal for this config source. If a property is specified in multiple config sources, the value
-     * in the config source with the highest ordinal takes precedence.
-     * For the config sources with the same ordinal value, the config source names will
-     * be used for sorting according to string sorting criteria.
-     * Note that this property only gets evaluated during ConfigSource discovery.
+     * Return the ordinal for this config source. If a property is specified in
+     * multiple config sources, the value in the config source with the highest
+     * ordinal takes precedence. For the config sources with the same ordinal value,
+     * the config source names will be used for sorting according to string sorting
+     * criteria. Note that this property only gets evaluated during ConfigSource
+     * discovery.
      *
      * The default ordinals for the default config sources:
-     <ol>
+     * <ol>
      * <li>System properties (ordinal=400)</li>
      * <li>Environment properties (ordinal=300)
-     *    <p>Some operating systems allow only alphabetic characters or an underscore(_), in environment variables.
-     *    Other characters such as ., /, etc may be disallowed.
-     *    In order to set a value for a config property that has a name containing such disallowed characters from an environment variable,
-     *    the following rules are used.
-     *    This ConfigSource searches for potentially 3 environment variables with a given property name (e.g. {@code "com.ACME.size"}):</p>
-     *        <ol>
-     *            <li>Exact match (i.e. {@code "com.ACME.size"})</li>
-     *            <li>Replace the character that is neither alphanumeric nor '_' with '_' (i.e. {@code "com_ACME_size"})</li>
-     *            <li>Replace the character that is neither alphanumeric nor '_' with '_' and convert to upper case 
-     *            (i.e. {@code "COM_ACME_SIZE"})</li>
-     *        </ol>
-     *    <p>The first environment variable that is found is returned by this ConfigSource.</p>
+     * <p>
+     * Some operating systems allow only alphabetic characters or an underscore(_),
+     * in environment variables. Other characters such as ., /, etc may be
+     * disallowed. In order to set a value for a config property that has a name
+     * containing such disallowed characters from an environment variable, the
+     * following rules are used. This ConfigSource searches for potentially 3
+     * environment variables with a given property name (e.g.
+     * {@code "com.ACME.size"}):
+     * </p>
+     * <ol>
+     * <li>Exact match (i.e. {@code "com.ACME.size"})</li>
+     * <li>Replace the character that is neither alphanumeric nor '_' with '_' (i.e.
+     * {@code "com_ACME_size"})</li>
+     * <li>Replace the character that is neither alphanumeric nor '_' with '_' and
+     * convert to upper case (i.e. {@code "COM_ACME_SIZE"})</li>
+     * </ol>
+     * <p>
+     * The first environment variable that is found is returned by this
+     * ConfigSource.
+     * </p>
      * </li>
      * <li>/META-INF/microprofile-config.properties (default ordinal=100)</li>
      * </ol>
      *
      *
-     * Any ConfigSource part of an application will typically use an ordinal between 0 and 200.
-     * ConfigSource provided by the container or 'environment' typically use an ordinal higher than 200.
-     * A framework which intends have values overwritten by the application will use ordinals between 0 and 100.
-     * The property "config_ordinal" can be specified to override the default value.
+     * Any ConfigSource part of an application will typically use an ordinal between
+     * 0 and 200. ConfigSource provided by the container or 'environment' typically
+     * use an ordinal higher than 200. A framework which intends have values
+     * overwritten by the application will use ordinals between 0 and 100. The
+     * property "config_ordinal" can be specified to override the default value.
      *
      * @return the ordinal value
      */
     default int getOrdinal() {
-        String configOrdinal = getValue(CONFIG_ORDINAL);
-        if(configOrdinal != null) {
+        final String configOrdinal = getValue(CONFIG_ORDINAL);
+        if (configOrdinal != null) {
             try {
                 return Integer.parseInt(configOrdinal);
-            }
-            catch (NumberFormatException ignored) {
+            } catch (final NumberFormatException ignored) {
 
             }
         }
