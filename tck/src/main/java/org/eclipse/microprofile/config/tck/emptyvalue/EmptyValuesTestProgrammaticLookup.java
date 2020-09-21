36217b8e1581e1f5971acb7553d09d81fff9c871
Emily Jiang <emijiang6@googlemail.com>
2020-09-21 15:12:20 +0100

    tidy up
    Signed-off-by: Emily Jiang <emijiang6@googlemail.com>

--- tck/src/main/java/org/eclipse/microprofile/config/tck/emptyvalue/EmptyValuesTestProgrammaticLookup.java
+++ tck/src/main/java/org/eclipse/microprofile/config/tck/emptyvalue/EmptyValuesTestProgrammaticLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Contributors to the Eclipse Foundation
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -41,7 +41,6 @@ public class EmptyValuesTestProgrammaticLookup extends Arquillian {
     @Deployment
     public static Archive deployment() {
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "emptyValues.jar")
-                .addClasses(EmptyValuesTest.class, EmptyValuesBean.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAsManifestResource(new StringAsset(
                                             "empty.string=" + "\n" +
