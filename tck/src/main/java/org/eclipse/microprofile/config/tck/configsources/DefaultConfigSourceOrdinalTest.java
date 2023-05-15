4fb0e55eac2392f50a0e123401bd0b0e6dca22f7
Tom Evans <tevans@uk.ibm.com>
2023-05-15 12:41:38 +0100

    Switch expected and actual values to match TestNG assertEquals
    Signed-off-by: Tom Evans <tevans@uk.ibm.com>

--- tck/src/main/java/org/eclipse/microprofile/config/tck/configsources/DefaultConfigSourceOrdinalTest.java
+++ tck/src/main/java/org/eclipse/microprofile/config/tck/configsources/DefaultConfigSourceOrdinalTest.java
@@ -86,14 +86,14 @@ public class DefaultConfigSourceOrdinalTest extends Arquillian {
 
     @Test
     public void testOrdinalForEnv() {
-        Assert.assertEquals("Bill", config.getValue("customer_name", String.class));
-        Assert.assertEquals(200, config.getConfigValue("customer_name").getSourceOrdinal());
+        Assert.assertEquals(config.getValue("customer_name", String.class), "Bill");
+        Assert.assertEquals(config.getConfigValue("customer_name").getSourceOrdinal(), 200);
     }
 
     @Test
     public void testOrdinalForSystemProps() {
-        Assert.assertEquals("Badminton", config.getValue("customer.hobby", String.class));
-        Assert.assertEquals(200, config.getConfigValue("customer.hobby").getSourceOrdinal());
+        Assert.assertEquals(config.getValue("customer.hobby", String.class), "Badminton");
+        Assert.assertEquals(config.getConfigValue("customer.hobby").getSourceOrdinal(), 200);
     }
 
 }
