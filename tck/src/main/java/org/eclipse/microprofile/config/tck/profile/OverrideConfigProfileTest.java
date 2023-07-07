8ec7abad94aa22b619c4a315b8fa4e2db625f617
Oliver Bertuch <o.bertuch@fz-juelich.de>
2023-07-07 09:14:52 +0200

    doc(tck): apply minor tweaks to OverrideConfigProfileTest as suggested per review

--- tck/src/main/java/org/eclipse/microprofile/config/tck/profile/OverrideConfigProfileTest.java
+++ tck/src/main/java/org/eclipse/microprofile/config/tck/profile/OverrideConfigProfileTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -38,7 +38,7 @@ import static org.hamcrest.Matchers.is;
 /**
  * Test cases for Config profile
  *
- * @author Emily Jiang
+ * @author Oliver Bertuch
  */
 public class OverrideConfigProfileTest extends Arquillian {
     @Deployment
